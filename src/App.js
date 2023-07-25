5a568a2aa731cf2e1aa150473995d11d7fa0a034
geerthana-j <geerthana.cs19@bitsathy.ac.in>
2023-07-25 09:12:50 +0530

    added basics

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherComponent from './WeatherComponent';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <WeatherComponent />
+    </div>
+  );
+}
+
+export default App;
